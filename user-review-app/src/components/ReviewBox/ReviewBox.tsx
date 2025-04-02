a1c6b20d0c337ca85ce2f87aa45a1896c07f209f
Chris Rolando <chris.rolando@outlook.com>
2025-04-02 13:03:26 -0600

    user-review-app {adding starts for improving UX }

--- user-review-app/src/components/ReviewBox/ReviewBox.tsx
+++ user-review-app/src/components/ReviewBox/ReviewBox.tsx
@@ -1,9 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import styles from "./review-box.module.css";
 
 interface Review {
   text: string;
   author: string;
   avatar?: string;
+  rating?: number;
 }
 
 interface ReviewBoxProps {
@@ -11,20 +14,28 @@ interface ReviewBoxProps {
 }
 
 export const ReviewBox = ({ review }: ReviewBoxProps) => {
+  const stars = Array.from({ length: 5 }, (_, index) => (
+    <FontAwesomeIcon
+      key={index}
+      icon={faStar}
+      className={index < (review.rating ?? 0) ? styles["star-filled"] : styles["star-empty"]}
+    />
+  ));
+
   return (
     <div className={styles["review-container"]}>
-    <div className={styles["review-header"]}>
-      {review.avatar && (
-        <img
-          src={review.avatar}
-          alt={`Avatar de ${review.author}`}
-          className={styles["review-avatar"]}
-        />
-      )}
-      <h2 className={styles["review-author"]}>- {review.author}</h2>
+      <div className={styles["review-header"]}>
+        {review.avatar && (
+          <img
+            src={review.avatar}
+            alt={`Avatar de ${review.author}`}
+            className={styles["review-avatar"]}
+          />
+        )}
+        <h2 className={styles["review-author"]}>{review.author}</h2>
+      </div>
+      <div className={styles["review-rating"]}>{stars}</div>
+      <p className={styles["review-text"]}>"{review.text}"</p>
     </div>
-    <p className={styles["review-text"]}>"{review.text}"</p>
-  </div>
-
   );
 };
--- user-review-app/src/helpers/reviewsHelper.ts
+++ user-review-app/src/helpers/reviewsHelper.ts
@@ -2,22 +2,26 @@ interface Review {
   text: string;
   author: string;
   avatar?: string;
+  rating: number; 
 }
 
 export const reviews: Review[] = [
   {
     text: "El servicio de esta empresa es excepcional. ¡Muy recomendado!",
     author: "Juan Pérez",
-    avatar: "/images/avatar.webp"
+    avatar: "/images/avatar.webp",
+    rating: 5
   },
   {
     text: "Muy satisfecho con la calidad de los productos.",
     author: "Ana Gómez",
-    avatar: "/images/avatar.webp"
+    avatar: "/images/avatar.webp",
+    rating: 5
   },
   {
     text: "Excelente atención al cliente.",
     author: "Carlos López",
-    avatar: "/images/avatar.webp"
+    avatar: "/images/avatar.webp",
+    rating: 4
   }
 ];
